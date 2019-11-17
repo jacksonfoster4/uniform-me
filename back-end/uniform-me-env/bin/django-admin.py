b760f70d21a4977882afb0ecd48ed84bfd3df278
Jackson Foster <jacksonfoster444@gmail.com>
2019-11-17 13:20:22 -0800

    init repo

---
+++ back-end/uniform-me-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jacksonfoster/Code/projects/uniform-me/back-end/uniform-me-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
