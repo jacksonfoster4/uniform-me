ceff476e4468db50391bf28ec41efaac5742b672
Jackson Foster <jacksonfoster444@gmail.com>
2019-11-22 21:00:50 -0800

    fixed prod env

--- back-end/uniform_me_api/manage.py
+++ back-end/uniform_me_api/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uniform_me_api.development_settings')
+    if not os.environ.get("DJANGO_SETTINGS_MODULE"):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uniform_me_api.development_settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
